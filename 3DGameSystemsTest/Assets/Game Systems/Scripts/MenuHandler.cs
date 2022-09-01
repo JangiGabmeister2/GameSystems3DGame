db8b55b828e02a4f0b6e0333e0df0317121fa292
JangiGabmeister2 <109714078+JangiGabmeister2@users.noreply.github.com>
2022-09-01 17:51:04 +1000

    added menu and audio control
    added audio and chaos
    added menu system
    buttons switch between menus and scenes

--- 3DGameSystemsTest/Assets/Game Systems/Scripts/MenuHandler.cs
+++ 3DGameSystemsTest/Assets/Game Systems/Scripts/MenuHandler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuHandler : MonoBehaviour
 {
@@ -61,9 +62,17 @@ public class MenuHandler : MonoBehaviour
         }
     }
 
+    public void ChangeScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
     public void QuitGame()
     {
+#if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
     }
 }
 
