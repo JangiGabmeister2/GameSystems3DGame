b40a80d0f1fef57e9ba8bb7ace2dde1c9d1eda73
JangiGabmeister2 <109714078+JangiGabmeister2@users.noreply.github.com>
2022-09-01 13:35:06 +1000

    added menu
    added menu states
    added buttons which changes menus
    added button which closes the game

---
+++ 3DGameSystemsTest/Assets/Game Systems/Scripts/MenuHandler.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuHandler : MonoBehaviour
+{
+    public GameObject[] panels;
+
+    public MenuStates menuState;
+
+    public void Start()
+    {
+        ChangePanel(0);
+    }
+    public void Update()
+    {
+        if (menuState == MenuStates.AnyKey)
+        {
+            if (Input.anyKey)
+            {
+                ChangePanel(1);
+            }
+        }
+    }
+
+    public void ChangePanel(int value)
+    {
+        menuState = (MenuStates)value;
+
+        switch (menuState)
+        {
+            case MenuStates.AnyKey:
+                for (int i = 0; i < panels.Length; i++)
+                {
+                    panels[i].SetActive(false); //turns them all off
+                }
+                panels[0].SetActive(true); //then turns this one on again
+                break;
+            case MenuStates.MainMenu:
+                for (int i = 0; i < panels.Length; i++)
+                {
+                    panels[i].SetActive(false);
+                }
+                panels[1].SetActive(true);
+                break;
+            case MenuStates.Options:
+                for (int i = 0; i < panels.Length; i++)
+                {
+                    panels[i].SetActive(false);
+                }
+                panels[2].SetActive(true);
+                break;
+            default:
+                for (int i = 0; i < panels.Length; i++)
+                {
+                    panels[i].SetActive(false);
+                }
+                panels[0].SetActive(true);
+                menuState = MenuStates.AnyKey;
+                break;
+        }
+    }
+
+    public void QuitGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+}
+
+public enum MenuStates
+{
+    AnyKey,
+    MainMenu,
+    Options
+}
